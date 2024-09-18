25d7491903ec7ef3c6c30582e98adaa74fc0173b
drinor <drinorsutaj@gmail.com>
2024-09-18 17:06:30 +0200

    Initial commit (setup gradle)

---
+++ Worttrainer/src/main/java/sutaj/worttrainer/WorttrainerApplication.java
@@ -0,0 +1,13 @@
+package sutaj.worttrainer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorttrainerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorttrainerApplication.class, args);
+	}
+
+}
---
+++ Worttrainer/src/test/java/sutaj/worttrainer/WorttrainerApplicationTests.java
@@ -0,0 +1,13 @@
+package sutaj.worttrainer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorttrainerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
