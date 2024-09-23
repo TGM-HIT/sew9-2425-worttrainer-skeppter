11f815d975546c84c420a20afc3e6f3d0197d944
drinor <drinorsutaj@gmail.com>
2024-09-23 12:44:15 +0200

    Basic implementation of Statistics.java

---
+++ src/main/java/sutaj/worttrainer/Statistics.java
@@ -0,0 +1,50 @@
+package sutaj.worttrainer;
+
+public class Statistics {
+    private int total;
+    private int correct;
+    private int incorrect;
+
+    public Statistics() {
+        this.total = 0;
+        this.correct = 0;
+        this.incorrect = 0;
+    }
+
+    public void incrementTotal() {
+        total++;
+    }
+
+    public void incrementCorrect() {
+        correct++;
+    }
+
+    public void incrementIncorrect() {
+        incorrect++;
+    }
+
+    public int getTotal() {
+        return total;
+    }
+
+    public int getCorrect() {
+        return correct;
+    }
+
+    public int getIncorrect() {
+        return incorrect;
+    }
+
+    // Neue Setter
+    public void setTotal(int total) {
+        this.total = total;
+    }
+
+    public void setCorrect(int correct) {
+        this.correct = correct;
+    }
+
+    public void setIncorrect(int incorrect) {
+        this.incorrect = incorrect;
+    }
+}
